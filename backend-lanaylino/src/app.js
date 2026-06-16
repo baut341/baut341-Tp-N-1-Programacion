2856398516980e3d7dfb4ec17e50404b433736d3
Bautista Badaracco <bauti1011@hotmail.com>
2026-06-16 18:44:03 -0300

    Backend (igual al original, solo lo subo por las dudas)

---
+++ backend-lanaylino/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+
+import usuarioRoutes from "./routes/usuario.routes";
+import loginRoutes from "./routes/login.routes";
+import productoRoutes from "./routes/producto.routes";
+
+const app = express();
+
+
+app.set("port", 4000);
+
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+//Routes
+app.use("/api",usuarioRoutes);
+app.use("/api",loginRoutes);
+app.use("/api", productoRoutes);
+
+
+
+
+
+
+export default app;
